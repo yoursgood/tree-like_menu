256439e8c7fe3e9863122babae64f23806ed44aa
yoursgood <87009538+yoursgood@users.noreply.github.com>
2023-03-27 21:22:00 +0700

    Инициализация проекта

---
+++ tree_menu/urls.py
@@ -0,0 +1,25 @@
+"""tree_menu URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from draw_menu.views import HomeView, BlogView
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('blog/', BlogView.as_view(), name='blog'),
+    path('', HomeView.as_view(), name='home'),
+]
